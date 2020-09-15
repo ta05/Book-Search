b633067d5636e6f151d1f0504f41acd1f8c19e9a
Tolu Alimi <tolualimi05@gmail.com>
2020-09-14 19:55:08 -0500

    First commit

---
+++ client/build/precache-manifest.1e1ef252b5468114da02cfd4e85e35a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0c6fb1473a5afeac61ca7b72e88b52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4abb2ec04100506baa1d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f38d715d37ac3e33e46c",
+    "url": "/static/js/2.f83db6a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f83db6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abb2ec04100506baa1d",
+    "url": "/static/js/main.650124b7.chunk.js"
+  },
+  {
+    "revision": "c04f05cffdb40f14153f",
+    "url": "/static/js/runtime-main.1aa6bfdc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
