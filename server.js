c4e369ca2a263715ba2e7e788e2e620eeef098c6
Tolu Alimi <tolualimi05@gmail.com>
2020-09-21 15:20:07 -0500

    Formatted server.js adding mongo uri

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 require('dotenv').config();
 
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+let uri = "mongodb://localhost/booksdb"
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    uri = process.env.MONGODB_URI;
 }
 
-// Define API routes here
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
 
-// Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
